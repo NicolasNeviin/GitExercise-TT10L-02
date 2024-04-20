249b7893a90f0d1eb2ea90a60beb0a91b293ab93
NicolasNeviin <167620964+NicolasNeviin@users.noreply.github.com>
2024-04-20 21:21:50 +0800

    Add files via upload

---
+++ egg catcher game python.py
@@ -0,0 +1,191 @@
+from itertools import cycle
+from random import randrange
+from tkinter import Canvas, Tk, messagebox, font, PhotoImage
+import pygame
+import pygame.mixer
+import os
+
+import pygame, sys
+from button import Button
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("font.ttf", size)
+
+def play():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+        
+
+       
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+    
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                    main_menu()
+
+        pygame.display.update()
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("Play Rect.png"), pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("Quit Rect.png"), pos=(640, 550), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON,  QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                    
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+                    
+               
+        pygame.display.update()
+
+canvas_width = 800
+canvas_height = 400
+
+root = Tk()
+root.title("Fruit Assasin")
+c = Canvas(root, width=canvas_width, height=canvas_height, background="blue")
+  
+
+c.create_rectangle(-5, canvas_height-100, canvas_width+5, canvas_height+5, fill='pink', width=0)
+c.create_oval(-80, -80, 200, 120, fill='yellow', width=0)
+c.pack()
+#Music
+pygame.init()
+music=pygame.mixer.music.load('Game music.mp3')
+collect_noise=pygame.mixer.Sound('collect noise.wav')
+pygame.mixer.music.play(-1)
+
+color_cycle = cycle(["light blue", "light green", "light pink", "light yellow", "light cyan"])
+egg_width = 45
+egg_height = 55
+egg_score = 10
+egg_speed = 500
+egg_interval = 4000
+difficulty = 0.95
+catcher_color = "blue"
+catcher_width = 100
+catcher_height = 100
+catcher_startx = canvas_width / 2 - catcher_width / 2
+catcher_starty = canvas_height - catcher_height - 20
+catcher_startx2 = catcher_startx + catcher_width
+catcher_starty2 = catcher_starty + catcher_height
+
+catcher = c.create_arc(catcher_startx, catcher_starty, catcher_startx2, catcher_starty2, start=200, extent=140, style="arc", outline=catcher_color, width=3)
+game_font = font.nametofont("TkFixedFont")
+game_font.config(size=18)
+
+
+score = 0
+score_text = c.create_text(10, 10, anchor="nw", font=game_font, fill="darkblue", text="Score: "+ str(score))
+
+lives_remaining = 3
+lives_text = c.create_text(canvas_width-10, 10, anchor="ne", font=game_font, fill="darkblue", text="Lives: "+ str(lives_remaining))
+
+eggs = []
+
+def create_egg():
+    x = randrange(10, 740)
+    y = 40
+    new_egg = c.create_oval(x, y, x+egg_width, y+egg_height, fill=next(color_cycle), width=0)
+    eggs.append(new_egg)
+    root.after(egg_interval, create_egg)
+
+def move_eggs():
+    for egg in eggs:
+        (eggx, eggy, eggx2, eggy2) = c.coords(egg)
+        c.move(egg, 0, 10)
+        if eggy2 > canvas_height:
+            egg_dropped(egg)
+    root.after(egg_speed, move_eggs)
+
+def egg_dropped(egg):
+    eggs.remove(egg)
+    c.delete(egg)
+    lose_a_life()
+    if lives_remaining == 0:
+        messagebox.showinfo("Game Over!", "Final Score: "+ str(score))
+        root.destroy()
+
+def lose_a_life():
+    global lives_remaining
+    lives_remaining -= 1
+    c.itemconfigure(lives_text, text="Lives: "+ str(lives_remaining))
+
+def check_catch():
+    (catcherx, catchery, catcherx2, catchery2) = c.coords(catcher)
+    for egg in eggs:
+        (eggx, eggy, eggx2, eggy2) = c.coords(egg)
+        if catcherx < eggx and eggx2 < catcherx2 and catchery2 - eggy2 < 40:
+            eggs.remove(egg)
+            c.delete(egg)
+            increase_score(egg_score)
+            collect_noise.play()
+    root.after(100, check_catch)
+
+def increase_score(points):
+    global score, egg_speed, egg_interval
+    score += points
+    egg_speed = int(egg_speed * difficulty)
+    egg_interval = int(egg_interval * difficulty)
+    c.itemconfigure(score_text, text="Score: "+ str(score))
+
+def move_left(event):
+    (x1, y1, x2, y2) = c.coords(catcher)
+    if x1 > 0:
+        c.move(catcher, -20, 0)
+
+def move_right(event):
+    (x1, y1, x2, y2) = c.coords(catcher)
+    if x2 < canvas_width:
+        c.move(catcher, 20, 0)
+
+c.bind("<Left>", move_left)
+c.bind("<Right>", move_right)
+c.focus_set()
+root.after(1000, create_egg)
+root.after(1000, move_eggs)
+root.after(1000, check_catch)
+root.mainloop()
